4b1d0950c1769e32ef7b47d05e54ace61db4d1f9
Garrett-Tobin <gtobin779@gmail.com>
2025-03-26 13:07:55 -0400

    Added setup Pins

---
+++ Python Files/functions.py
@@ -0,0 +1,23 @@
+import time
+import tkinter as tk
+# import lgpio
+
+import PinNumbers
+
+def setupPins(chip):
+    # Setup Outputs
+    # lgpio.gpio_claim_output(chip, CONVST)
+    # lgpio.gpio_claim_output(chip, SDI)
+    # lgpio.gpio_claim_output(chip, RST)
+    # lgpio.gpio_claim_output(chip, SDIN)
+    # lgpio.gpio_claim_output(chip, SYNC)
+    # lgpio.gpio_claim_output(chip, RESET)
+    # lgpio.gpio_claim_output(chip, TRI_STATE)
+    
+    # Setup Inputs
+    # lgpio.gpio_claim_input(chip, RVS)
+    # lgpio.gpio_claim_input(chip, ALRM)
+    # lgpio.gpio_claim_input(chip, SDO_O)
+    
+    print("TESTING setupPins")
+
