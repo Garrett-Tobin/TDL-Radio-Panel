02cb18826e6112703d5e81f9238c5faf1ce60751
PI USER <gtobin779@gmail.com>
2025-03-27 12:56:28 -0400

    Added Button and Label for grabbing DelayTime

--- Python Files/functions.py
+++ Python Files/functions.py
@@ -1,25 +1,30 @@
 import time
 import tkinter as tk
-# import lgpio
+import lgpio
 
 import PinNumbers
 
 def setupPins(chip):
     # Setup Outputs
-    # lgpio.gpio_claim_output(chip, CONVST)
-    # lgpio.gpio_claim_output(chip, SDI)
-    # lgpio.gpio_claim_output(chip, RST)
-    # lgpio.gpio_claim_output(chip, SDIN)
-    # lgpio.gpio_claim_output(chip, SYNC)
-    # lgpio.gpio_claim_output(chip, RESET)
-    # lgpio.gpio_claim_output(chip, TRI_STATE)
+    lgpio.gpio_claim_output(chip, PinNumbers.CONVST)
+    lgpio.gpio_claim_output(chip, PinNumbers.SDI)
+    lgpio.gpio_claim_output(chip, PinNumbers.RST)
+    lgpio.gpio_claim_output(chip, PinNumbers.SDIN)
+    lgpio.gpio_claim_output(chip, PinNumbers.SYNC)
+    lgpio.gpio_claim_output(chip, PinNumbers.RESET)
+    lgpio.gpio_claim_output(chip, PinNumbers.TRI_STATE)
     
     # Setup Inputs
-    # lgpio.gpio_claim_input(chip, RVS)
-    # lgpio.gpio_claim_input(chip, ALRM)
-    # lgpio.gpio_claim_input(chip, SDO_O)
-    
-    print("TESTING setupPins")
+    lgpio.gpio_claim_input(chip, PinNumbers.RVS)
+    lgpio.gpio_claim_input(chip, PinNumbers.ALRM)
+    lgpio.gpio_claim_input(chip, PinNumbers.SDO_O)
+    # End of setupPins(chip)
+
+def grabDelayTime(label, spinBox):
+    # Grab the Delay Time from the SpinBox
+    time = spinBox.get() # Grab the spinBox Value
+    label.config(text=f"Current Delay Time: {time}") # Update the Label
+    return time
 
 def createTDLWindow(windowTitle):
     windowTitle.title("TDL-Radio-Panel") # Set Window Title
--- Python Files/main.py
+++ Python Files/main.py
@@ -1,5 +1,5 @@
 # Import Libraries
-# import lgpio
+import lgpio
 import tkinter as tk
 import time
 
@@ -9,41 +9,51 @@ import PinNumbers
 # Import Functions
 import functions
 
+def handleDelayButton():
+    delayTime = functions.grabDelayTime(delayDisplay_lbl, delayTime_SpinBox)
+
 ADC_ON = False
 
-TDLwindow = tk.Tk() # Set up the main window
+try:
+    TDLwindow = tk.Tk() # Set up the main window
 
-# Set up all GPIO Pins as input or output
-chip = 12
-functions.setupPins(chip)
+    # Set up all GPIO Pins as input or output
+    chip = lgpio.gpiochip_open(0) # Open the GPIO chip
+    functions.setupPins(chip)
 
-# Setup and Configure the Tkinter Window
+    # Setup and Configure the Tkinter Window
 
-functions.createTDLWindow(TDLwindow)
+    functions.createTDLWindow(TDLwindow)
 
-# Create Title Label
-title_lbl = tk.Label(TDLwindow, text="T.D.L-Radio-Panel P.O.C.", font=("Times New Roman", 24))
-title_lbl.pack(pady=20)
+    # Create Title Label
+    title_lbl = tk.Label(TDLwindow, text="T.D.L-Radio-Panel P.O.C.", font=("Times New Roman", 24))
+    title_lbl.pack(pady=20)
 
-# Delay Time Capture Label
-delayCapture_lbl = tk.Label(TDLwindow, text="Input Desired Delay Time", font=("Times New Roman", 16))
-delayCapture_lbl.pack(pady=10)
+    # Delay Time Capture Label
+    delayCapture_lbl = tk.Label(TDLwindow, text="Input Desired Delay Time", font=("Times New Roman", 16))
+    delayCapture_lbl.pack(pady=10)
 
-# SpinBox to Input Delay Time
-delayTime_SpinBox = tk.Spinbox(TDLwindow, from_=1, to_=10, width=5, font=("Times New Roman", 12))
-delayTime_SpinBox.pack(pady=5)
+    # SpinBox to Input Delay Time
+    delayTime_SpinBox = tk.Spinbox(TDLwindow, from_=1, to_=10, width=5, font=("Times New Roman", 12))
+    delayTime_SpinBox.pack(pady=5)
 
-# Button to Set Delay Time
+    # Button to Set Delay Time
+    setDelayTime_btn = tk.Button(TDLwindow, text="Set Desired Delay Time", font=("Times New Roman", 16), command=handleDelayButton)
+    setDelayTime_btn.pack(pady=20)
 
+    # Label to Display Current Delay Time
+    delayDisplay_lbl = tk.Label(TDLwindow, text="Current Delay Time: 0", font=("Times New Roman", 16))
+    delayDisplay_lbl.pack(pady=3)
 
-# On Indicator (Just for Test Purposes Right Now
-indicator_lbl = tk.Label(TDLwindow, text="Off", font=("Times New Roman", 16), bg="red", fg="white")
-indicator_lbl.pack(side="right", padx=20)
+    # On Indicator (Just for Test Purposes Right Now
+    indicator_lbl = tk.Label(TDLwindow, text="Off", font=("Times New Roman", 16), bg="red", fg="white")
+    indicator_lbl.pack(side="right", padx=20)
 
-# Create Quit Button
-quit_btn = tk.Button(TDLwindow, text="Quit Program", command=TDLwindow.destroy)
-quit_btn.pack(side="bottom", pady=20)
+    # Create Quit Button
+    quit_btn = tk.Button(TDLwindow, text="Quit Program", command=TDLwindow.destroy)
+    quit_btn.pack(side="bottom", pady=20)
 
-TDLwindow.mainloop() # Run the interface in a loop
-
-# End File
+    TDLwindow.mainloop() # Run the interface in a loop
+finally:
+    lgpio.gpiochip_close(chip) # Close the GPIO chip
+    # End File
