f502f5e4a8715e3418b86562bd073ffb59f55b16
PI USER <gtobin779@gmail.com>
2025-04-03 15:06:20 -0400

    fixed pinNumbers to accomadate for SDI

--- Python Files/PinNumbers.py
+++ Python Files/PinNumbers.py
@@ -3,14 +3,14 @@
 # Input Pins
 RVS = 17 #ADC
 ALRM = 27 #ADC
-SDO_O = 22 # ADC
+SDO_O = 9 # ADC
 
 
 # Output Pins
 CONVST = 23 # ADC
 SDI = 24 # ADC
 RST = 25 # ADC
-SDIN = 16 # DAC 
+SDIN = 10 # DAC 
 SYNC = 26 # DAC
 RESET = 6 # DAC
 TRI_STATE = 5 # Oscillator
